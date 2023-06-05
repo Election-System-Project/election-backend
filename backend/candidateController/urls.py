b5fcaf3650317835c8c85b030fa831535c403574
Malik Hinnawi <malikhinnawi01@gmail.com>
2023-06-05 19:47:16 +0300

    Edited candidateController

--- backend/candidateController/urls.py
+++ backend/candidateController/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("candidates/", views.candidate_list_view),
-    path("candidates/<int:pk>/", views.candidate_details_view),
-    path("candidates/create/", views.candidate_create_view)
+    path("candidates/", views.approved_candidate_list_view, name= "approved_candidates"),
+    path("candidates/<int:pk>/", views.candidate_details_view, name = "detail_view"),
+    path("candidates/create/", views.candidate_create_view, name = "create_view")
 ]
